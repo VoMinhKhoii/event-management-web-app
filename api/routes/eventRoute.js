4ca4a9de7336abbd251043ab23279572120bfa8f
nmquan1 <nmquanrmit@gmail.com>
2025-04-15 20:03:25 +0700

    [ADD]: eventRoute

---
+++ api/routes/eventRoute.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { getAllEvent, getEvent, createEvent, updateEvent, deleteEvent } from '../controllers/eventController.js';
+import { verifyToken} from '../middleware/verifyToken.js';
+
+const router = express.Router();
+
+router.get('/', getAllEvent);
+router.get('/:id', getEvent);
+router.post('/' ,createEvent);
+router.put('/:id', updateEvent);
+router.delete('/:id', deleteEvent);
+
+export default router;
