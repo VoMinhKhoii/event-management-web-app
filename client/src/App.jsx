2e5948af733e9d1f6bb30a6f8159df666526261d
nmquan1 <nmquanrmit@gmail.com>
2025-04-17 16:10:43 +0700

    [FIX]: Restructure App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,55 +1,8 @@
 import React from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import HomePage from './pages/Home/HomePage.jsx';
-import Calendar from './pages/Calendar/Calendar.jsx';
-import EventDetails from './components/EventDetails.jsx';
-import Profile from './pages/Profile/ProfilePage.jsx';
-import CreateEvent from './pages/CreateEvent/CreateEvent.jsx';
-import LandingPage from './pages/Landing/LandingPage.jsx';
-import LoginPage from './pages/Login/LoginPage.jsx';
-import SignUpPage from './pages/SignUp/SignUpPage.jsx';
-import NotificationPage from './pages/Notification/NotificationPage.jsx';
+import { RouterProvider } from 'react-router-dom';
+import router from './routes'; // This uses index.jsx by default
 
-function App () {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <LandingPage />
-    },
-    {
-      path: "/login",
-      element: <LoginPage />
-    },
-    {
-      path: "/signup",
-      element: <SignUpPage />
-    },
-    {
-      path: "/home",
-      element: <HomePage />
-    },
-    {
-      path: "/calendar",
-      element: <Calendar />
-    },
-    {
-      path: "/event/:id",
-      element: <EventDetails />
-    },
-    {
-      path: "/profile",
-      element: <Profile />
-    },
-    {
-      path: "/create-event",
-      element: <CreateEvent />
-    },
-    {
-      path: "/notifications",
-      element: <NotificationPage />
-    }
-  
-  ]);
+function App() {
   return <RouterProvider router={router} />;
 }
 
