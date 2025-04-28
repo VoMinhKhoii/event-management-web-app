87e1b98bb84ef65df703fafb18040de19bbb1918
koromaki88 <officialngannhien@gmail.com>
2025-04-29 02:02:59 +0700

    feat: invite & handle invitations API

--- api/routes/eventRoute.js
+++ api/routes/eventRoute.js
@@ -1,18 +1,21 @@
 import express from 'express';
-import { getAllEvent, getEvent, createEvent, updateEvent, deleteEvent, requestToJoinEvent, handleJoinRequest } from '../controllers/eventController.js';
+import { getAllEvent, getEvent, createEvent, updateEvent, deleteEvent } from '../controllers/eventController.js';
+import { requestToJoinEvent, handleJoinRequest } from '../controllers/eventRequestController.js';
+import { inviteToEvent, handleInvitation } from '../controllers/eventInvitationController.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/', verifyToken, getAllEvent);
-router.get('/:id', verifyToken, getEvent);
+router.get('/:eventId', verifyToken, getEvent);
 router.post('/', verifyToken, createEvent);
-router.put('/:id', verifyToken, updateEvent);
-router.delete('/:id', verifyToken, deleteEvent);
+router.put('/:eventId', verifyToken, updateEvent);
+router.delete('/:eventId', verifyToken, deleteEvent);
 
-router.post('/:eventId/request-join', verifyToken, requestToJoinEvent);             // Request to join public event
-router.put('/:eventId/requests/:requestId', verifyToken, handleJoinRequest);        // Organizer approve/reject join request
-// router.post('/:eventId/invite', verifyToken, inviteToEvent);                        // Invite to join private event
-// router.put('/:eventId/invitations/:invitationId', verifyToken, handleInvitation);   // Receiver accept/reject invitation
+// TODO: Ability to cancel request/invitation, GET all requests/invitation associated with a user
+router.post('/:eventId/request-join', verifyToken, requestToJoinEvent);                 // Request to join public event
+router.put('/:eventId/requests/:requestId', verifyToken, handleJoinRequest);            // Organizer approve/reject join request
+router.post('/:eventId/invite', verifyToken, inviteToEvent);                         // Invite to join private event
+router.put('/:eventId/invitations/:invitationId', verifyToken, handleInvitation);    // Receiver accept/reject invitation
 
 export default router;
