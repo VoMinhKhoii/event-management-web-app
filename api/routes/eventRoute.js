553f09ac27d3df32904e6c86b852d9056da98d0c
koromaki88 <officialngannhien@gmail.com>
2025-04-28 22:57:00 +0700

    restore eventRoute

--- api/routes/eventRoute.js
+++ api/routes/eventRoute.js
@@ -4,11 +4,15 @@ import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
-
 router.get('/', verifyToken, getAllEvent);
-router.get('/:id',verifyToken, getEvent);
-router.post('/',verifyToken, createEvent);
-router.put('/:id',verifyToken, updateEvent);
-router.delete('/:id',verifyToken, deleteEvent);
+router.get('/:id', verifyToken, getEvent);
+router.post('/', verifyToken, createEvent);
+router.put('/:id', verifyToken, updateEvent);
+router.delete('/:id', verifyToken, deleteEvent);
+
+router.post('/:eventId/request-join', verifyToken, requestToJoinEvent);             // Request to join public event
+router.put('/:eventId/requests/:requestId', verifyToken, handleJoinRequest);        // Organizer approve/reject join request
+// router.post('/:eventId/invite', verifyToken, inviteToEvent);                        // Invite to join private event
+// router.put('/:eventId/invitations/:invitationId', verifyToken, handleInvitation);   // Receiver accept/reject invitation
 
 export default router;
