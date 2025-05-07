7a773b99e47996fa030c95539d5a30108d4399e4
KevlarAinzworth <142222595+KevlarAinzworth@users.noreply.github.com>
2025-05-07 23:15:49 +0700

    remove resend invite

--- client/src/components/EventDetails.jsx
+++ client/src/components/EventDetails.jsx
@@ -559,18 +559,6 @@ const EventDetails = () => {
                         </svg>
                       </button>
                     )}
-
-                    {invitation.status === 'rejected' && (
-                      <button
-                        // onClick={() => handleResendInvite(invitation._id || invitation.id)}
-                        className="ml-2 p-1 text-gray-400 hover:text-gray-600"
-                        title="Resend invitation"
-                      >
-                        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
-                        </svg>
-                      </button>
-                    )}
                   </div>
                 </div>
               ))}
